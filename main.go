4affd74f01331ba0a10006162dcc7b78ec14211a
initrdmk <initrdmk@gmail.com>
2015-11-25 21:04:37 +0800

    now runs without features

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"encoding/json"
 	"log"
 	"math/rand"
+	"strings"
+	"errors"
 )
 
 import "io/ioutil"
@@ -17,7 +19,10 @@ import _ "github.com/go-sql-driver/mysql"
 // -------------------- Redis --------------------
 var redis *pool.Pool
 func init_redis() {
-	redis, err := pool.New("tcp", "localhost:6379", 10)
+	var err error
+	host := os.Getenv("REDIS_HOST")
+	port := os.Getenv("REDIS_PORT")
+	redis, err = pool.New("tcp", host + ":" + port, 10)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -34,7 +39,40 @@ func redis_add_token(token string, uid int) {
 	redis.Put(conn)
 }
 
+func redis_add_cart(token string, cart_id string) {
+	conn, err := redis.Get()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if conn.Cmd("PUT", token, cart_id).Err != nil {
+		log.Fatal(err)
+	}
+	redis.Put(conn)
+}
 
+type Cart struct {
+	cart_id string
+	token string
+	count int
+}
+
+func redis_get_cart(token string, cart_id string) *Cart {
+	return nil
+}
+
+func redis_add_to_cart(cart_id string, food_id int, count int) {
+	
+}
+
+func redis_commit_order(cart *Cart) error {
+	//err := redis_commmit_order(cart)
+	return nil
+}
+
+func redis_get_order(order string) (string, bool) {
+	return "1", true
+
+}
 func redis_set(key string, value string) {
 
 }
@@ -106,6 +144,9 @@ type BodyCart struct {
 	food_id int
 	count int
 }
+type BodyCartId struct {
+	cart_id string
+}
 func carts(w http.ResponseWriter, r *http.Request) {
 	token, err := get_token(w, r)
 	if err != nil {
@@ -133,7 +174,9 @@ func carts(w http.ResponseWriter, r *http.Request) {
 			}`))
 			return
 		}
+		cart_id := strings.TrimPrefix(r.RequestURI, "/carts/")
 		patch_carts(w, token, cart_id, body)
+
 	} else {
 		// TODO: comment out all redundant checks
 		w.Write([]byte("Wrong Method"))
@@ -141,8 +184,8 @@ func carts(w http.ResponseWriter, r *http.Request) {
 }
 
 func post_carts(w http.ResponseWriter, token string) {
-	cart_id = "cart1d" + RandStringBytes(11)
-	redis_add_cart(card_id, token)
+	cart_id := "cart1d" + RandStringBytes(11)
+	redis_add_cart(token, cart_id)
 	w.Write([]byte(fmt.Sprintf(`{
 		"cart_id": %d
 	}`, cart_id)))
@@ -151,10 +194,10 @@ func post_carts(w http.ResponseWriter, token string) {
 func patch_carts(
 	w http.ResponseWriter,
 	token string,
-	cart_id int,
+	cart_id string,
 	body BodyCart) {
 
-	cart = redis_get_cart(cart_id)
+	cart := redis_get_cart(token, cart_id)
 	if cart == nil {
 		w.Write([]byte(`{
 			"code": "CART_NOT_FOUND",
@@ -179,7 +222,7 @@ func patch_carts(
 		return
 	}
 
-	_, ok := foods_cache[food_id]
+	_, ok := foods_cache[body.food_id]
 	if !ok {
 		w.WriteHeader(404)
 		w.Write([]byte(`{
@@ -188,7 +231,7 @@ func patch_carts(
 		}`))
 		return
 	}
-	redis_add_to_cart(cart_id, food_id, count)
+	redis_add_to_cart(cart_id, body.food_id, body.count)
 	w.WriteHeader(204)
 }
 
@@ -197,7 +240,7 @@ type BodyOrder struct {
 	cart_id string
 }
 func post_orders(w http.ResponseWriter, token string, body BodyOrder) {
-	cart = redis_get_cart(body.cart_id)
+	cart := redis_get_cart(token, body.cart_id)
 	if cart == nil {
 		w.WriteHeader(404)
 		w.Write([]byte(`{
@@ -214,14 +257,15 @@ func post_orders(w http.ResponseWriter, token string, body BodyOrder) {
 		}`))
 		return
 	}
-	if token.done {
+	//if token.done {
+	if false {
 		w.WriteHeader(403)
 		w.Write([]byte(`{
 			"code": "ORDER_OUT_OF_LIMIT",
 			"message": "每个用户只能下一单"
 		}`))
 	}
-	err := redis_commmit_order(cart)
+	err := redis_commit_order(cart)
 	if  err != nil {
 		w.WriteHeader(403)
 		w.Write([]byte(`{
@@ -239,16 +283,16 @@ func post_orders(w http.ResponseWriter, token string, body BodyOrder) {
 func get_orders(w http.ResponseWriter, token string) {
 	order, ok := token2order[token];
 	if ok {
-		w.Write(order)
+		w.Write([]byte(order))
 		return
 	}
-	order, ok = redis_get("done_order" + token)
+	order, ok = redis_get_order("done_order" + token)
 	if ok {
 		token2order[token] = order
-		w.Write(order)
+		w.Write([]byte(order))
 		return
 	}
-	w.Write("")
+	w.Write([]byte(""))
 }
 
 func orders(w http.ResponseWriter, r *http.Request) {
@@ -287,27 +331,28 @@ func orders(w http.ResponseWriter, r *http.Request) {
 
 // -------------------- Food --------------------
 type Food struct {
-	food_id int
+//	food_id int
 	price int
 	stock int
 }
-var foods_cache map[id]Food
+
+var foods_cache map[int]Food
 // TODO: weaken the consistency
 func foods(w http.ResponseWriter, r *http.Request) {
 	// check GET
-	token, err := get_token(w, r)
+	_, err := get_token(w, r)
 	if err != nil {
 		return
 	}
 	ret := `[
 	`
 	for id, food := range foods_cache {
-		ret += `{"id": ` + id + `, "price": ` + food.price + `, "stock": ` + food.stock + `},
-		`
+		ret += fmt.Sprintf(`{"id": %d, "price": %d, "stock": %d},
+		`, id, food.price, food.stock)
 	}
 	ret += `]`
 
-	w.Write(ret.to_bytes())
+	w.Write([]byte(ret))
 }
 
 // TODO: make things async
@@ -328,6 +373,7 @@ type UserP struct {
 
 var tokens [11001]string
 var userps map[string]UserP
+var token2order map[string]string
 
 func gen_token() {
 	for i := 0; i<11001; i++ {
@@ -341,7 +387,8 @@ func cache_foods() {
 	var id int
 	var price int
 	var stock int
-	rows, err := db.Query("SELECT id, name, pass FROM food")
+	foods_cache = map[int]Food{}
+	rows, err := db.Query("SELECT id, stock, price FROM food")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -357,13 +404,15 @@ func cache_foods() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println("food cached")
 }
 func cache_users() {
 	var id int
 	var name string
 	var pass string
+	userps = map[string]UserP{}
 
-	rows, err := db.Query("SELECT id, name, pass FROM user")
+	rows, err := db.Query("SELECT id, name, password FROM user")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -379,13 +428,22 @@ func cache_users() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println("user cached");
 }
 func init_mysql() {
-	db, err := sql.Open("mysql", "user:password@/dbname")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	var err error
+	db, err = sql.Open("mysql",
+		user + ":" + pass + "@tcp(" +
+		host + ":" + port + ")/" + name)
 	if err != nil {
 		panic(err.Error())
 	}
-	defer db.Close()
+	//defer db.Close()
 	err = db.Ping()
 	if err != nil {
 		panic(err.Error())
@@ -421,14 +479,19 @@ func main() {
 	http.HandleFunc("/carts", carts)
 	http.HandleFunc("/carts/:cart_id", carts)
 	http.HandleFunc("/orders", orders)
-	http.HandleFunc("/admin/orders", admin_orders)
+	//http.HandleFunc("/admin/orders", admin_orders)
 
+	log.Println("serving...")
 	http.ListenAndServe(addr, nil)
 }
 
-func get_token(w http.ResponseWrite, r *http.Request) (string, error) {
-	token, ok := r.URL.Query()["access_token"]
-	if ok && token != "" {
+func redis_valid_token(token string) bool {
+	return true
+}
+
+func get_token(w http.ResponseWriter, r *http.Request) (string, error) {
+	token := r.URL.Query().Get("access_token")
+	if token != string("") {
 		valid := redis_valid_token(token)
 		if valid {
 			return token, nil
@@ -446,7 +509,7 @@ func get_token(w http.ResponseWrite, r *http.Request) (string, error) {
 		"code": "INVALID_ACCESS_TOKEN",
 		"message": "无效的令牌"
 	}`))
-	return nil, errors.New()
+	return "", errors.New("")
 }
 
 /*
